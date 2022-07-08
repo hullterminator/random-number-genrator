3a7f6c839bb027289e5e8d2a3f2d74e18f675d9a
hullterminator <78999873+hullterminator@users.noreply.github.com>
2022-07-08 14:06:52 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# Program to generate a random number between 0 and 9
+
+# importing the random module
+import random
+i='y'
+while i=='y':
+  i=input('do you want more numbers?')
+  print(random.randint(0,99))
+else:
+  exit()
